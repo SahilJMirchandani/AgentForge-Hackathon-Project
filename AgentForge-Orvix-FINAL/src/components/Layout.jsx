15ea877ed434ff7705ed494a05e64d77c7e3c9a1
Sahil Mirchandani <sahilmirchandani@gmail.com>
2026-09-19 23:13:03 +0530

    Connect responsive navigation controls

--- AgentForge-Orvix-FINAL/src/components/Layout.jsx
+++ AgentForge-Orvix-FINAL/src/components/Layout.jsx
@@ -1,15 +1,17 @@
 import { Outlet } from 'react-router-dom'
+import { useState } from 'react'
 import Sidebar from './Sidebar'
 import Topbar from './Topbar'
 import { useAppStore } from '../store/useAppStore'
 
 export default function Layout() {
   const userName = useAppStore((s) => s.userName)
+  const [mobileOpen, setMobileOpen] = useState(false)
   return (
     <div className="flex min-h-screen bg-canvas">
-      <Sidebar />
+      <Sidebar mobileOpen={mobileOpen} onClose={() => setMobileOpen(false)} />
       <div className="flex-1 flex flex-col min-w-0">
-        <Topbar userName={userName} />
+        <Topbar userName={userName} onMenu={() => setMobileOpen(true)} />
         <main className="flex-1 p-6 min-w-0">
           <Outlet />
         </main>
