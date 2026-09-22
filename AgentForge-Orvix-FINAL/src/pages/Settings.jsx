73ab991b9436f6aa0a0b96a5df44bce3a5303e4d
Sahil Mirchandani <sahilmirchandani@gmail.com>
2026-09-22 22:53:52 +0530

    Fix Settings Gmail connection state import

--- AgentForge-Orvix-FINAL/src/pages/Settings.jsx
+++ AgentForge-Orvix-FINAL/src/pages/Settings.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAppStore } from '../store/useAppStore'
 import { apiRequest } from '../api'
